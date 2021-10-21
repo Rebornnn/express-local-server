b0c106989de89ae98990203557f54fcdaa00b240
libowen <libowen@bonree.com>
2021-10-21 17:50:57 +0800

    fix

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const { port = 8080, proxyTable = [] } = require("../index.js");
 
 const app = express();
-app.use("/", express.static("./dist")); // 设置静态资源访问路径
+app.use("/", express.static("./public")); // 设置静态资源访问路径
 proxyTable.forEach((item) =>
   app.use(
     createProxyMiddleware(item.api, {
