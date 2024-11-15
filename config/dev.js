9550dd7a13758f6dfc43237739de45ddbf55ca76
libowen <libowen@bonree.com>
2024-11-15 17:03:29 +0800

    feat: 增加上下文

--- config/dev.js
+++ config/dev.js
@@ -2,18 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const connectHistoryApiFallback = require("connect-history-api-fallback");
-const { port = 8080, proxyTable = [] } = require("../index.js");
+const { port, proxyTable = [], contextPath } = require("../index.js");
 
 const app = express();
-app.use(cors())
-app.use("/", connectHistoryApiFallback());
-app.use("/", express.static("./public")); // 设置静态资源访问路径
+app.use(cors());
+app.use(`${contextPath}`, connectHistoryApiFallback());
+app.use(`${contextPath}`, express.static("./static/iam")); // 设置静态资源访问路径
+app.use(`${contextPath}/static/iam`, express.static("./static/iam")); // 设置静态资源访问路径
+app.use(`${contextPath}/static/apm`, express.static("./static/apm"));
+app.use(`${contextPath}/static/analysis`, express.static("./static/analysis"));
+app.use(`${contextPath}/static/rum`, express.static("./static/rum"));
+app.use(`${contextPath}/static/alert`, express.static("./static/alert"));
+app.use(`${contextPath}/static/test`, express.static("./static/test"));
+// app.use("/static/bonree-design", express.static("./static/bonree-design"));
+// app.use("/static/bonree-design-plus", express.static("./static/bonree-design-plus"));
 proxyTable.forEach((item) =>
   app.use(
     createProxyMiddleware(item.api, {
       target: item.target, // 目标服务器 host
       changeOrigin: true, // // 默认false，是否需要改变原始主机头为目标URL
       ws: true, // 是否代理websockets
+      pathRewrite: (path) => {
+        return path.replace(contextPath, "");
+      }, // 重写路径
     })
   )
 );
--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+// 上下文路径
+const contextPath = '';
+
 module.exports = {
-  port: 8081,
-  host: "localhost",
+  port: 8080,
+  // host: "localhost",
+  host: "http://192.168.1.156",
   proxyTable: [
     {
-      api: "/rest",
+      api: `${contextPath}/rest`,
       // 邹海涛IP
       // target: 'http://192.168.1.97:8082',
       // 杨伟康IP
       // target: 'http://192.168.1.20:8082',
       // 陆文飞IP
       // target: 'http://192.168.1.205:8082',
-      // 非SSO
-      target: 'http://10.241.110.35:5555',
+      // 陈强
+      // target: 'http://192.168.1.232:8081',
+      // 测试
+      // target: 'http://10.241.110.11:9102',
+      target: "http://10.241.110.13:58899",
+      // 邹海涛外网ip
+      // target: "http://223.76.185.147:9797",
+    },
+    {
+      api: `${contextPath}/open`,
+      // 邹海涛IP
+      // target: 'http://192.168.1.97:8082',
+      // 杨伟康IP
+      // target: 'http://192.168.1.20:8082',
+      // 陆文飞IP
+      // target: 'http://192.168.1.205:8082',
+      // 陈强
+      // target: 'http://192.168.1.232:8081',
+      // 测试
+      // target: 'http://10.241.110.11:9102',
+      target: "http://10.241.110.13:58899",
       // 邹海涛外网ip
       // target: "http://223.76.185.147:9797",
     },
   ],
+  contextPath
 };
